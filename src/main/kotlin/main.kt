baaae4ae8ce4d3bce23abe4961cf4a64873021c1
Csáktornyai Ádám József (Adam) <jockahun@gmail.com>
2021-05-19 15:53:14 +0200

    Added more documentation to matrices

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,6 @@
+import math.BaseTransformation
 import math.Matrix
 
 fun main(args: Array<String>) {
-    val m1 = Matrix(3,
-    3, 1, 2,
-    2, 3, 1)
-    val m2 = Matrix(3, 2) { y, x -> (x + y).toDouble() }
 
-    m1 * m2
 }
